2c7a02bc5f7303ea7f0f818e0f39ef8323cf3566
Gilded Honour <me@gildedhonour.com>
2014-11-13 19:53:59 +0800

    improvements

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ require 'json'
 PAGE_COUNTS_FILE_URL_TEMPLATE = 'https://dumps.wikimedia.org/other/pagecounts-raw/%{year}/%{year}-%{month}/pagecounts-%{year}%{month}%{day}-%{hour}0000.gz'
 MIN_YEAR = 2007
 HOURS_PER_DAY = 23
-CSV_FILE_NAME = 'wiki_urls.csv'
+CSV_FILE_NAME = 'wiki_encoded_urls2.csv'
 REDIS_DB_NAME = 'wikipedia_pagecounts'
 REDIS_LOGIN = ''
 REDIS_PASSWORD = ''
@@ -24,37 +24,29 @@ def main
   end 
   
   abort('[ERROR] Start date has to be less or equal to the current date.') if date_start >= DateTime.now.to_date
-
-
   to_date = date_end ? date_end : date_start
   (0..(to_date - date_start).to_i).to_a.each do |x|
     date_iter = date_start + x
     month_formatted = '%02d' % date_iter.month
     day_formatted = '%02d' % date_iter.day
     
-    # local_file_names = (0..HOURS_PER_DAY).map do |hour|
-    local_file_names = (0..5).map do |hour|
+    local_file_names = (0..HOURS_PER_DAY).map do |hour|
+    # local_file_names = (0..0).map do |hour| #todo - remove - 2 files
       hour_formatted = '%02d' % hour
       url = PAGE_COUNTS_FILE_URL_TEMPLATE % { year: date_iter.year, month: month_formatted, day: day_formatted, hour: hour_formatted }
       save_file(url)
     end
 
-    #todo
-    # local_file_names = ['pagecounts_text1.gz'] #todo - remove
-    
-            binding.pry
-
-
-    local_file_names.each do |file_name|
-      puts(" - Processing #{file_name}...")
-      page_counts_str = uncompress_gz_file(file_name) #todo - path
+    local_file_names.each do |full_file_name|
+      puts(" - [Processing]...")
+      page_counts_str = uncompress_gz_file(full_file_name)
       page_counts_hash = process_gz_file_content(page_counts_str)
 
       source_links = read_and_parse_csv(CSV_FILE_NAME)
       date_iter_formatted = date_iter.strftime("%Y%m%d").to_sym
       process_pagecounts_dump_file(source_links, page_counts_hash, date_iter_formatted)
       
-      puts(" - [OK] Done processing the file #{file_name}\n\n")
+      puts(" - [OK]\n\n")
     end
   end
 end
@@ -66,75 +58,61 @@ def save_file(url, repeat_count = 3)
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.request_get(uri.path) do |response|
-    
-    # binding.pry
-
     case response
     when Net::HTTPNotFound
       puts(' - [Error] 404: not found')
       return nil
-
     when Net::HTTPForbidden
       raise ' - [Error] 403: forbidden, exhausted all retry attempts' if repeat_count <= 0 
       puts('- [Error] 403: forbidden, one more attempt...')
       sleep(SLEEP_SECONDS)  
       return save_file(url, repeat_count - 1)
-
     when Net::HTTPClientError
       puts(" - [Error] client error: #{response.inspect}")
       return nil
-
     when Net::HTTPOK
       temp_file = Tempfile.new("download-#{file_name}")
       temp_file.binmode
-
-              # binding.pry
-
-
       size = 0
       progress = 0
       total = response.header["Content-Length"].to_i
       total_mb = total / 1024 / 1024
-
-      # binding.pry
-
-
       response.read_body do |chunk|
         temp_file << chunk
         size += chunk.size
         new_progress = (size * 100) / total
-        print("\rDownloading %s %3d%% of ≈ #{total_mb}Mb" % [file_name, new_progress]) unless new_progress == progress
+        print("\r - [Downloading] the dump file %s %3d%% of ≈ #{total_mb}Mb " % [file_name, new_progress]) unless new_progress == progress
         progress = new_progress
       end
 
-      puts(" - [OK] Downloaded")
-
+      puts("[OK]")
       temp_file.close
       File.unlink(full_path) if File.exists?(full_path)
       FileUtils.mkdir_p(File.dirname(full_path))
       FileUtils.mv(temp_file.path, full_path, force: true)
-
-      full_path
     end
   end
 
+  full_path
 rescue Exception => e
   File.unlink(full_path) if File.exists?(full_path)
   puts(" - [Error]: #{e.message}")
-  raise 'Failed to download file'
+  raise "Failed to download the file #{url}"
 end
 
 def read_and_parse_csv(csv_file_name)
+  language_first_index = 7
   result = {}
   CSV.foreach(csv_file_name) do |row|
     first_url_index = row.index { |x| x.start_with?('http://') }
     row.drop(first_url_index).each do |maybe_link|
       link_result = {}
+      
       if maybe_link.start_with?('http://')
-        lng = maybe_link[7..8] #todo - hardcode
+        lng = maybe_link[language_first_index..language_first_index + 1]
         title = maybe_link[maybe_link.rindex('/') + 1..-1]
-        next if title.include?('?curid=') #todo - /index.html?curid=91365 - take only ?curid=91365
-        key = "#{lng}_#{title}"
+        title_formatted = parse_csv_row_title(title)
+        key = "#{lng}_#{title_formatted}"
         result[key] = row[0]
       else
         prev_key = result.keys[-1]
@@ -142,30 +120,39 @@ def read_and_parse_csv(csv_file_name)
         result.delete(prev_key)
         result[new_key] = row[0]
       end
+
     end
   end
 
   result
 end
 
+def parse_csv_row_title(title)
+  ind = title.index('?curid=')
+  ind ? title[ind..title.length] : title
+end
+
 def connect_to_redis
-  Redis.new(host: REDIS_IP, port: 6379, db: REDIS_DB_NAME)
+  @redis ||= Redis.new(host: REDIS_IP, port: 6379, db: REDIS_DB_NAME)
 end
 
 def uncompress_gz_file(file_name)
+  print(' - [Uncompressing] the dump file ')
   infile = open(file_name)
   gz = Zlib::GzipReader.new(infile)
   res = gz.read
   gz.close
+  puts('[OK]')
   res
 end
 
 def process_gz_file_content(input)
+  title_starting_index = 3
   res = {}
   input.each_line do |x|
-    lng = x[0 .. 1].downcase
-    ind1 = x.index(' ', 3)
-    title = x[3 ... ind1]
+    lng = x[0..1].downcase
+    ind1 = x.index(' ', title_starting_index)
+    title = x[title_starting_index...ind1]
     key = "#{lng}_#{title}"
     ind2 = x.index(' ', ind1 + 1)
     val = x[ind1 + 1 ... ind2]
@@ -174,32 +161,12 @@ def process_gz_file_content(input)
 
   res
 end
-
-def uncompress_and_process_gz_file(file_name)
-  res = {}
-  infile = open(file_name)
-  gz = Zlib::GzipReader.new(infile)
-  gz.each_line do |x|
-    lng = x[0 .. 1].downcase
-    ind1 = x.index(' ', 3)
-    title = x[3 ... ind1]
-    key = "#{lng}_#{title}"
-    ind2 = x.index(' ', ind1 + 1)
-    val = x[ind1 + 1 ... ind2]
-    res[key] = val
-  end
-
-  res
-end
-
 
 # # todo - filter for only the records we need - wikipedia records
 def process_pagecounts_dump_file(input, page_counts_hash, date_key)
+  print(' - [Parsing] ')
   result = {}
   input.each do |lng_title, intern_id|
-
-    # binding.pry
-
     if page_counts_hash[lng_title]
       count = page_counts_hash[lng_title].to_i
       lng = lng_title[0..1].to_sym
@@ -234,19 +201,18 @@ def process_pagecounts_dump_file(input, page_counts_hash, date_key)
     end
   end
 
+  puts('[OK]')
   save_to_redis(result)
 end
 
 def save_to_redis(input)
+  print(' - [Saving] to redis ')
   redis = connect_to_redis()
   i = 0
   input.each do |k, date_lng_val|
     maybe_json = redis.get(k)
     date_key = date_lng_val.keys[0]
     if maybe_json
-
-      # binding.pry
-
       old_data = JSON.parse(maybe_json, { symbolize_names: true })
       new_data = old_data.merge(date_lng_val) do |key, v1, v2| 
         v1.merge(v2) { |key, v1, v2| v1 + v2 } 
@@ -258,17 +224,11 @@ def save_to_redis(input)
       hash_formatted[k] = date_lng_val
       redis.set(k, hash_formatted.to_json) #todo
     end
-    
-    # puts(" -- The key #{k} has been inserted to or updated in redis")
+
     i += 1
   end
 
-  puts(" - Total of #{i} keys have been saved to or updated in redis")
+  puts("[OK] (#{i} rows affected)")
 end
 
 main()
-# puts('Done')
-
-# v.each do |date, lng_count_val|
-
-    # end
